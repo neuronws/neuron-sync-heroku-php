aa42397109f8cf2fa63cd6b52fad126fa1529e45
Jonatan Linares <jlinares@neuron.ws>
2014-12-18 16:27:31 +0100

    Fix binaries paths

--- nws_sync_boot.sh
+++ nws_sync_boot.sh
@@ -1,2 +1,2 @@
-/app/vendor/nws_sync_setup >> /tmp/nws_sync_out 2>> /tmp/nws_sync_err
-/app/vendor/nws_sync_watch_boot.sh >> /tmp/nws_sync_out 2>> /tmp/nws_sync_err &
+/app/vendor/bin/nws_sync_setup >> /tmp/nws_sync_out 2>> /tmp/nws_sync_err
+/app/vendor/bin/nws_sync_watch_boot.sh >> /tmp/nws_sync_out 2>> /tmp/nws_sync_err &
--- nws_sync_watch_boot.sh
+++ nws_sync_watch_boot.sh
@@ -1,5 +1,5 @@
 while true; do
-  /app/vendor/nws_sync_watch
+  /app/vendor/bin/nws_sync_watch
   
   sleep 5
 done
