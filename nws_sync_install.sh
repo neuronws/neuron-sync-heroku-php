987fb5a3056003453de514279b39c5e90ebd4293
Jonatan Linares <jlinares@neuron.ws>
2014-12-18 20:06:06 +0100

    Relative paths

--- nws_sync_install.sh
+++ nws_sync_install.sh
@@ -1,7 +1,5 @@
-cd /app
-
 if [ ! -d ".profile.d" ]; then
   mkdir .profile.d
 fi
 
-ln -s /app/vendor/bin/nws_sync_boot.sh /app/.profile.d/nws_sync_boot.sh
+cp vendor/bin/nws_sync_boot.sh .profile.d/nws_sync_boot.sh
